f8757dc17037543a82fbce61780b9abf9774ed8e
Kirillabaskin <kirillabaskin99@gmail.com>
2019-04-26 00:53:04 +0300

    Добавил пояснительные комментарии

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    private int weight;
-    private ArrayList<Cargo> cargos;
-    private CargoLogistUnit max=new CargoLogistUnit();
+    private int weight; // глобальная переменная веса машины
+    private ArrayList<Cargo> cargos; // глобальныйы лист грузов
+    private CargoLogistUnit max=new CargoLogistUnit(); // максимальный элемент в графе из грузов
 
     public static void main(String[] args){
         Main main= new Main();
@@ -13,7 +13,7 @@ public class Main {
 
     private void go(){
         try {
-            Scanner sc = new Scanner(System.in);
+            Scanner sc = new Scanner(System.in); // читаем из консоли и единственные проблемы могут возникнуть только при непрвильном вводе
             weight = Integer.parseInt(sc.nextLine());
             String allCargos = sc.nextLine();
             String[] cargosStrings = allCargos.split(" ");
@@ -27,23 +27,23 @@ public class Main {
             unit.findMax();
             max.showPath();
         }catch (Exception e){
-            System.out.print("Некорректно введены данные");
+            System.out.print("Некорректно введены данные"); // поэтому такой блок catch
         }
     }
 
-    private class CargoLogistUnit{
-        private CargoLogistUnit prev;
-        private ArrayList<CargoLogistUnit> next = new ArrayList<>();
-        private Cargo unit;
-        private boolean isEnd=false;
+    private class CargoLogistUnit{ // по сути это внутренний класс для вершины графа-дерева
+        private CargoLogistUnit prev;  // ссылка на вершину родителя. Для корневой вершины равен null
+        private ArrayList<CargoLogistUnit> next = new ArrayList<>(); // лист потомков
+        private Cargo unit;// сам груз
+        private boolean isEnd=false;// переменная указывающая что эта вершина не действительна из-за перегруза или перебора всех возможных грузов
         public CargoLogistUnit(){
             prev=null;
-        }
-        private CargoLogistUnit(CargoLogistUnit parent,Cargo cargo){
+        }// единственный публичный коструктор для инициализации дерева
+        private CargoLogistUnit(CargoLogistUnit parent,Cargo cargo){ // внутренний конструктор
             prev=parent;
             unit=cargo;
         }
-        private boolean isContains(Cargo cargo){
+        private boolean isContains(Cargo cargo){ // метод для прохода по ветке до встречи соотвествия
             if(unit == cargo)
                 return true;
             else if(prev==null)
@@ -52,21 +52,21 @@ public class Main {
                 return prev.isContains(cargo);
         }
 
-        private int weighThere(){
+        private int weighThere(){ // сумма массы всех грузов от начала дерева до данной вершины
             if(prev!=null)
                 return unit.getWeight()+prev.weighThere();
             else
                 return 0;
         }
 
-        private int costThere(){
+        private int costThere(){ // тоже самое но только для цены
             if(prev!=null)
                 return unit.getCost()+prev.costThere();
             else
                 return 0;
         }
 
-        public void nextLevel(){
+        public void nextLevel(){ // создание следующего поколения вершин
             if(weighThere()>weight){
                 isEnd=true;
                 return ;
@@ -80,7 +80,7 @@ public class Main {
             }
         }
 
-        public boolean isEndPoint() {
+        public boolean isEndPoint() { // метод для того что бы узнать есть ли потомки у данной вершины
             for(CargoLogistUnit unit:next){
                 if(!unit.isEnd)
                     return false;
@@ -88,7 +88,7 @@ public class Main {
             return true;
         }
 
-        public void findMax(){
+        public void findMax(){ // метод который находит максимальную вершину и заменяет глобальную переменную на нее
             for(CargoLogistUnit unit:next){
                 if(unit.weighThere()<=weight & unit.isEndPoint()) {
                     if (unit.costThere()> max.costThere())
@@ -99,7 +99,7 @@ public class Main {
             }
         }
 
-        public void showPath(){
+        public void showPath(){ // просто показывает грузы в данной ветке и сумму их стоимости
             if(prev!=null) {
                 System.out.print(unit.getName()+" ");
                 prev.showPath();
